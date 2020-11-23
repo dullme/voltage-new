3c0054455fe8ca49430545df05530a82177388dd
JINJIALEI <admin@dullme.com>
2020-11-23 16:31:31 +0800

    完成API接口

--- routes/web.php
+++ routes/web.php
@@ -33,16 +33,18 @@ Route::post('/test','IndexController@index');
 
 Route::get('/awtrix',function(){
     $today = \Carbon\Carbon::today()->toDateString();
+    $month = \Carbon\Carbon::today()->subMonth()->toDateString();
     $client = new Client();
-    $response = $client->get('https://hq.smm.cn/ajax/spot/history/201102250376/'.$today.'/'.$today);
+    $url = "https://m.smm.cn/get_old_price_chart?priceType=products&ID=201102250376&startTime={$month}&endTime={$today}";
+    $response = $client->get($url);
     $data = json_decode($response->getBody()->getContents(), true);
-
-    $lastPrice = array_pop($data['data']);
-    $highs = $lastPrice['highs'];
-    $low = $lastPrice['low'];
-    $average = $lastPrice['average'];
-    $vchange = $lastPrice['vchange'];
-    $renew_date = $lastPrice['renew_date'];
+    $lastPrice = array_pop($data['Data']);
+    $highs = $lastPrice['Highs'];
+    $low = $lastPrice['Low'];
+    $average = $lastPrice['Average'];
+    $vchange = $lastPrice['Vchange'];
+    $renew_date = $lastPrice['RenewDate'];
+    
     return response()->json([
         'data'=>['text' => "$highs~$low $average +$vchange $renew_date"]
     ]);
